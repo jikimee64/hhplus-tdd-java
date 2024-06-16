32ad274f86091fe09656eeba388aa37e774315d2
jikimee64 <jikimee64@gmail.com>
2024-06-16 21:20:36 +0900

    feat: PointController 완성

--- src/main/java/io/hhplus/tdd/point/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -1,7 +1,9 @@
 package io.hhplus.tdd.point.controller;
 
+import io.hhplus.tdd.point.application.ChargePointService;
 import io.hhplus.tdd.point.domain.PointHistory;
 import io.hhplus.tdd.point.domain.UserPoint;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/point")
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final ChargePointService chargePointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -42,7 +46,7 @@ public class PointController {
             @PathVariable long userId,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return chargePointService.charge(userId, amount);
     }
 
     /**
