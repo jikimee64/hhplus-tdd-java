c2150df74c29c2defa8756ab1a990b0b9c36d801
jikimee64 <jikimee64@gmail.com>
2024-06-16 13:16:13 +0900

    feat: 포인트 충전시 1_000_000이 초과할경우 충전 실패

--- src/main/java/io/hhplus/tdd/point/application/ChargePointCommand.java
+++ src/main/java/io/hhplus/tdd/point/application/ChargePointCommand.java
@@ -1,11 +1,9 @@
 package io.hhplus.tdd.point.application;
 
-import lombok.Getter;
-
-@Getter
 public record ChargePointCommand(long pointId, long amount) {
 
     private static final long MIN_AMOUNT = 100;
+    private static final long MAX_AMOUNT = 1_000_000;
     private static final long AMOUNT_UNIT = 100;
 
     public ChargePointCommand {
@@ -19,6 +17,10 @@ public record ChargePointCommand(long pointId, long amount) {
 
         if (amount % AMOUNT_UNIT != 0) {
             throw new IllegalArgumentException("100원 단위로 충전 가능합니다.");
+        }
+
+        if (amount > MAX_AMOUNT) {
+            throw new IllegalArgumentException("1,000,000원을 초과하여 충전 할 수 없습니다.");
         }
     }
 
--- src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.point.application;
 
 import io.hhplus.tdd.point.domain.UserPoint;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +14,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * 통합 테스트 케이스
  * <p>
  * - 포인트 충전
- * - 100원 단위로 충전이 가능하다.
- * - 100원 미만일 시 충전에 실패한다
- * - 100원 단위가 아닐경우 충전에 실패한다
- * - 최대 1_000_000원까지 충전 가능하다.
- * - 충전 성공 내역을 기록한다
- * - 1_000_000원을 초과한 경우 충전할 수 없다.
- * - 충전 실패 내역을 기록한다
+     * - 100원 단위로 충전이 가능하다.
+     * - 100원 미만일 시 충전에 실패한다
+     * - 100원 단위가 아닐경우 충전에 실패한다
+     * - 1_000_000원이 초과할 경우 충전할 수 없다.
+     * - 충전 성공 및 실패 내역을 기록한다
  * - 포인트 사용
- * - 최소 100원 이상 1_000_000원 이하까지 사용할 수 있다.
- * - 사용 성공 내역을 기록한다
- * - 잔액이 부족할 경우 사용할 수 없다.
- * - 사용 실패 내역을 기록한다
+     * - 최소 100원 이상 1_000_000원 이하까지 사용할 수 있다.
+     * - 사용 성공 내역을 기록한다
+     * - 잔액이 부족할 경우 사용할 수 없다.
+     * - 사용 실패 내역을 기록한다
  * - 포인트 조회
- * - 포인트 조회에 성공한다
+     * - 포인트 조회에 성공한다
  * - 포인트 내역 조회
- * - 포인트 내역 조회에 성공한다
+     * - 포인트 내역 조회에 성공한다
  */
 @SpringBootTest
 class PointServiceTest {
@@ -72,6 +71,18 @@ class PointServiceTest {
         assertThatThrownBy(() -> new ChargePointCommand(pointId, amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("100원 단위로 충전 가능합니다.");
+    }
+
+    @Test
+    void 포인트_충전시_1_000_000원이_초과할_경우_충전할_수_없다() {
+        // given
+        long pointId = 1L;
+        long amount = 1_000_100L;
+
+        // when & then
+        assertThatThrownBy(() -> new ChargePointCommand(pointId, amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1,000,000원을 초과하여 충전 할 수 없습니다.");
     }
 
 }
