2bb3ca2a2d3f75545570b4df31d0d394c999dec5
jikimee64 <jikimee64@gmail.com>
2024-06-16 12:34:14 +0900

    chore: 도메인 repository 인터페이스 생성 및 persistence 구현체 연결

--- src/main/java/io/hhplus/tdd/point/domain/PointHistoryRepository.java
+++ src/main/java/io/hhplus/tdd/point/domain/PointHistoryRepository.java
@@ -5,5 +5,4 @@ import java.util.List;
 public interface PointHistoryRepository {
     PointHistory insert(long userId, long amount, TransactionType type, long updateMillis);
     List<PointHistory> selectAllByUserId(long userId);
-    PointHistory selectOneByUserId(long userId);
 }
