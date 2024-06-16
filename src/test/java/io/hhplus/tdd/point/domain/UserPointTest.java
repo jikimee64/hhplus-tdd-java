4a08906344faea4a8e2ceb9ae290f4ffa0cb5800
jikimee64 <jikimee64@gmail.com>
2024-06-16 21:53:01 +0900

    feat: 잔고가 부족할 경우, 포인트 사용은 실패

--- src/test/java/io/hhplus/tdd/point/domain/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/domain/UserPointTest.java
@@ -2,12 +2,13 @@ package io.hhplus.tdd.point.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class UserPointTest {
 
     @Test
-    void 사용자_포인트_감소() {
+    void 사용자_포인트_사용_성공() {
         // given
         UserPoint userPoint = new UserPoint(1L, 1000L, System.currentTimeMillis());
         long usePoint = 100L;
@@ -17,6 +18,19 @@ class UserPointTest {
 
         // then
         assertThat(900L).isEqualTo(result.point());
+    }
+
+    @Test
+    void 사용자_포인트_사용_실패(){
+        // given
+        UserPoint userPoint = new UserPoint(1L, 1000L, System.currentTimeMillis());
+        long usePoint = 1001L;
+
+        // when & then
+        assertThatThrownBy(() -> userPoint.usePoint(usePoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("남은 포인트 잔액이 부족하여 포인트를 사용할 수 없습니다.");
+
     }
 
 }
