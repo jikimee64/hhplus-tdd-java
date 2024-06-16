3f1a9aea441154ef6cba0e16ef3f3bfcf6c0caed
jikimee64 <jikimee64@gmail.com>
2024-06-16 13:07:07 +0900

    feat: 포인트 충전 100원 단위가 아닐 경우 실패

--- src/main/java/io/hhplus/tdd/point/application/ChargePointCommand.java
+++ src/main/java/io/hhplus/tdd/point/application/ChargePointCommand.java
@@ -3,21 +3,22 @@ package io.hhplus.tdd.point.application;
 import lombok.Getter;
 
 @Getter
-public class ChargePointCommand {
+public record ChargePointCommand(long pointId, long amount) {
 
-    private final long pointId;
+    private static final long MIN_AMOUNT = 100;
+    private static final long AMOUNT_UNIT = 100;
 
-    private final long amount;
-
-    public ChargePointCommand(long pointId, long amount) {
+    public ChargePointCommand {
         validateAmount(amount);
-        this.pointId = pointId;
-        this.amount = amount;
     }
 
     private void validateAmount(long amount) {
-        if(amount < 100){
+        if (amount < MIN_AMOUNT) {
             throw new IllegalArgumentException("100원 이상 충전 가능합니다.");
+        }
+
+        if (amount % AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException("100원 단위로 충전 가능합니다.");
         }
     }
 
