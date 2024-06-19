17cdb8f85a7457ae9d9b3d94e4b80e0ac949d9c9
jikimee64 <jikimee64@gmail.com>
2024-06-19 23:37:08 +0900

    feat: 포인트 충전 및 사용 동시성 테스트

--- src/main/java/io/hhplus/tdd/point/application/ChargePointService.java
+++ src/main/java/io/hhplus/tdd/point/application/ChargePointService.java
@@ -12,22 +12,29 @@ public class ChargePointService {
 
     private final UserPointRepository userPointRepository;
     private final PointHistoryRepository pointHistoryRepository;
+    private final PointServiceLock pointServiceLock;
 
     public UserPoint charge(long userId, long amount) {
-        UserPoint userPoint = userPointRepository.selectById(userId);
+        pointServiceLock.getLock();
+        try {
+            UserPoint userPoint = userPointRepository.selectById(userId);
 
-        ChargePointValidator chargePointValidator = new ChargePointValidator();
-        if(!chargePointValidator.validateAmount(amount)){
+            ChargePointValidator chargePointValidator = new ChargePointValidator();
+            if (!chargePointValidator.validateAmount(amount)) {
+                pointHistoryRepository.insert(
+                        userId, amount, TransactionType.CHARGE_FAIL, System.currentTimeMillis()
+                );
+                return userPoint;
+            }
+
+            userPoint = userPointRepository.insertOrUpdate(userId, userPoint.point() + amount);
             pointHistoryRepository.insert(
-                    userId, amount, TransactionType.CHARGE_FAIL, System.currentTimeMillis()
+                    userId, amount, TransactionType.CHARGE_SUCCESS, System.currentTimeMillis()
             );
             return userPoint;
+        } finally {
+            pointServiceLock.releaseLock();
         }
-        userPoint = userPointRepository.insertOrUpdate(userId, userPoint.point() + amount);
-        pointHistoryRepository.insert(
-                userId, amount, TransactionType.CHARGE_SUCCESS, System.currentTimeMillis()
-        );
-        return userPoint;
     }
 
 }
--- src/main/java/io/hhplus/tdd/point/application/UsePointService.java
+++ src/main/java/io/hhplus/tdd/point/application/UsePointService.java
@@ -11,27 +11,34 @@ public class UsePointService {
 
     private final UserPointRepository userPointRepository;
     private final PointHistoryRepository pointHistoryRepository;
+    private final PointServiceLock pointServiceLock;
 
     public UserPoint usePoint(long userId, long amount) {
-        UserPoint userPoint = userPointRepository.selectById(userId);
+        pointServiceLock.getLock();
+        try {
+            UserPoint userPoint = userPointRepository.selectById(userId);
 
-        UsePointValidator usePointValidator = new UsePointValidator();
-        if(!usePointValidator.validatePoint(amount)){
-            pointHistoryRepository.insert(
-                    userId, amount, TransactionType.USE_FAIL, System.currentTimeMillis()
-            );
-            return userPoint;
+            UsePointValidator usePointValidator = new UsePointValidator();
+            if (!usePointValidator.validatePoint(amount)) {
+                pointHistoryRepository.insert(
+                        userId, amount, TransactionType.USE_FAIL, System.currentTimeMillis()
+                );
+                return userPoint;
+            }
+            UserPoint usedUserPoint;
+
+            usedUserPoint = usePoint(userId, amount, userPoint);
+            userPointRepository.insertOrUpdate(usedUserPoint.userId(), usedUserPoint.point());
+            return usedUserPoint;
+        } finally {
+            pointServiceLock.releaseLock();
         }
-        UserPoint usedUserPoint = usePoint(userId, amount, userPoint);
-
-        userPointRepository.insertOrUpdate(usedUserPoint.userId(), usedUserPoint.point());
-        return usedUserPoint;
     }
 
     /**
      * - 포인트 사용시
-     *   - 사용할 수 있는 경우 사용 후 남은 포인트를 반환한다.
-     *   - 사용할 수 없는 경우 기존 포인트를 반환한다.
+     * - 사용할 수 있는 경우 사용 후 남은 포인트를 반환한다.
+     * - 사용할 수 없는 경우 기존 포인트를 반환한다.
      */
     private UserPoint usePoint(long userId, long amount, UserPoint userPoint) {
         UserPoint usedUserPoint = null;
@@ -40,7 +47,7 @@ public class UsePointService {
             pointHistoryRepository.insert(
                     userId, amount, TransactionType.USE_SUCCESS, System.currentTimeMillis()
             );
-        }catch (UsePointFailException ex){
+        } catch (UsePointFailException ex) {
             pointHistoryRepository.insert(
                     userId, amount, TransactionType.USE_FAIL, System.currentTimeMillis()
             );
